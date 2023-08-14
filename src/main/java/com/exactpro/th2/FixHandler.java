ca057141c15a5ee5d4fe3551896f230ecd41aba1
Denis Plotnikov <denis.plotnikov@exactprosystems.com>
2023-08-14 19:52:02 +0400

    open channel after logout

--- src/main/java/com/exactpro/th2/FixHandler.java
+++ src/main/java/com/exactpro/th2/FixHandler.java
@@ -321,6 +321,7 @@ public class FixHandler implements AutoCloseable, IHandler {
             context.send(CommonUtil.toEvent(String.format("Closing session %s. Is graceful disconnect: %b", channel.getSessionAlias(), !isUngracefulDisconnect)));
             try {
                 disconnect(!isUngracefulDisconnect);
+                channel.open().get();
             } catch (Exception e) {
                 context.send(CommonUtil.toErrorEvent(String.format("Error while ending session %s by user logout. Is graceful disconnect: %b", channel.getSessionAlias(), !isUngracefulDisconnect), e));
             }
